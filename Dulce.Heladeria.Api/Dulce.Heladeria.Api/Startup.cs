9f202e874c3206494d7c152b1bbe1efd214c044b
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-10-15 17:04:53 -0300

    PG2022-92 implementacion de swagger, autenticacion de endpoints y manejo global de errores

--- Dulce.Heladeria.Api/Dulce.Heladeria.Api/Startup.cs
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Api/Startup.cs
@@ -1,14 +1,19 @@
+using Dulce.Heladeria.Api.Helpers;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Dulce.Heladeria.Api
@@ -25,7 +30,42 @@ namespace Dulce.Heladeria.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options => 
+            {
+                options.SwaggerDoc("DulceHeladeriaApi", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Dulce Heladeria Api",
+                    Version = "v1",
+                    Description = "Grupo 02, Metodologia de Sistemas, Tecnicatura Universitaria en Programacion, UTN FRC",
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                    {
+                        Name = "Documentacion del proyecto",
+                        Url= new Uri("https://112902.atlassian.net/wiki/spaces/2022TUP2W2/")
+                    }
+                });
+
+                options.AddSecurityDefinition("Bearer",
+                   new OpenApiSecurityScheme
+                   {
+                       Description = "Autentificacion JWT (Bearer)",
+                       Type = SecuritySchemeType.Http,
+                       Scheme = "bearer"
+                   });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type=ReferenceType.SecurityScheme
+                        }
+                    }, new List<string>()
+                }
+                 });
+            });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,17 +75,44 @@ namespace Dulce.Heladeria.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+            }
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/DulceHeladeriaApi/swagger.json", "Dulce Heladeria Api");
+                options.RoutePrefix = "";
+            });
+
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         }
     }
 }
