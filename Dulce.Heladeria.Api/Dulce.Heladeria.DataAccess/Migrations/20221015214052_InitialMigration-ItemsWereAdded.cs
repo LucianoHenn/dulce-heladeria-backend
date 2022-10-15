2ac38484ac8c359d864e369352e8082168cd9d42
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-10-15 18:42:10 -0300

    PG2022-95 creacion migracion inicial

---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.DataAccess/Migrations/20221015214052_InitialMigration-ItemsWereAdded.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dulce.Heladeria.DataAccess.Migrations
+{
+    public partial class InitialMigrationItemsWereAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItemType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: false),
+                    ItemTypeId = table.Column<int>(nullable: false),
+                    MeasuringType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Item_ItemType_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "ItemType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_ItemTypeId",
+                table: "Item",
+                column: "ItemTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "ItemType");
+        }
+    }
+}
