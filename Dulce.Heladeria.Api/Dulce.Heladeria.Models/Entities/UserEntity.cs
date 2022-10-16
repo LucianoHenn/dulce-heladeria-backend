d8b7c8e1e349c35033a754138da8d25680b42de1
mdcaceres <you@example.com>
2022-10-16 14:47:27 -0300

    PG2022-110 entidad usuario agregada

---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Models/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dulce.Heladeria.Models.Entities
+{
+    internal class UserEntity
+    {
+    }
+}
---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Models/Enums/Roles.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dulce.Heladeria.Models.Enums
+{
+    public enum Roles
+    {
+        administrador,
+        vendedor
+    }
+}
