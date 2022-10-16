1d04daae243f56bd48736167de5d80696a3bffe6
mdcaceres <you@example.com>
2022-10-16 18:38:40 -0300

    PG2022-109 endpoint usuario

--- Dulce.Heladeria.Api/Dulce.Heladeria.Services/Manager/UserManager.cs
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Services/Manager/UserManager.cs
@@ -8,6 +8,7 @@ using Dulce.Heladeria.Repositories.IRepositories;
 using AutoMapper;
 using Dulce.Heladeria.Models.Entities;
 using Dulce.Heladeria.Models.UnitOfWork;
+using Dulce.Heladeria.Repositories.Repositories;
 
 namespace Dulce.Heladeria.Services.Manager
 {
@@ -16,12 +17,20 @@ namespace Dulce.Heladeria.Services.Manager
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
+
+        public UserManager(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+        }
         public async Task<bool> InsertUser(CreateUserDto user)
         {
             var userEntity = _mapper.Map<UserEntity>(user);
             await _userRepository.InsertAsync(userEntity);
-            var result = _unitOfWork.SaveChangesAsync();
-            return true;  
+            var result = await _unitOfWork.SaveChangesAsync();
+            return result == 1;  
         }
     }
 }
