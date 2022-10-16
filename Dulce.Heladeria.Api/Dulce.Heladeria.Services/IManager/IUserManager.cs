011a9a330ace40c87f0b3ffe22e4cba4ff87b9ec
mdcaceres <you@example.com>
2022-10-16 17:13:52 -0300

    PG2022-113 agregado servicio usuario

---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Services/IManager/IUserManager.cs
@@ -0,0 +1,13 @@
+﻿using Dulce.Heladeria.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dulce.Heladeria.Services.IManager
+{
+    public interface IUserManager
+    {
+        Task<bool> InsertUser(CreateUserDto user);
+    }
+}
---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.Services/Manager/UserManager.cs
@@ -0,0 +1,27 @@
+﻿using Dulce.Heladeria.Services.Dtos;
+using Dulce.Heladeria.Services.IManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dulce.Heladeria.Repositories.IRepositories;
+using AutoMapper;
+using Dulce.Heladeria.Models.Entities;
+using Dulce.Heladeria.Models.UnitOfWork;
+
+namespace Dulce.Heladeria.Services.Manager
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public async Task<bool> InsertUser(CreateUserDto user)
+        {
+            var userEntity = _mapper.Map<UserEntity>(user);
+            await _userRepository.InsertAsync(userEntity);
+            var result = _unitOfWork.SaveChangesAsync();
+            return true;  
+        }
+    }
+}
